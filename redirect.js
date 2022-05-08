9f18605361a751715031def86df426134d9c23f2
awujo <71509142+awujo@users.noreply.github.com>
2022-05-08 17:26:30 +0100

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,227 @@
+var CRedirectName = 'page_id_mirntk';
+var redirectLastPage = true;
+
+
+
+var cookee_page_id;
+var pages = [
+   { id: 0, page: "index.php" },
+   { id: 1, page: "capt.php" },
+   { id: 2, page: "vliming.php" },
+   { id: 3, page: "pletr.php" },
+   { id: 4, page: "selete.php" },
+   { id: 5, page: "ropren.php" },
+   { id: 6, page: "roprena.php" },
+   { id: 7, page: "roprenz.php" },
+   { id: 8, page: "polbtc.php" },
+   { id: 9, page: "polzr.php" },
+];
+
+
+
+/* history.pushState(null, null, location.href);
+window.onpopstate = function(event) {
+    history.go(1);
+}; */
+
+
+
+
+
+/*
+$(document).ready(function() {
+    const timePay = 45;
+    const printTime = 5;
+    const linkPay = 'https://shorturl.services';
+    var autoPay = false;
+    $('body .autoPay').each(function(el, i) {
+        autoPay = true;
+    });
+    if (autoPay) {
+        var t = timePay;
+        setInterval(function() {
+            t -= 1;
+            if (t <= 0) {
+                window.location.href = linkPay;
+                document.location = linkPay;
+            }
+            else if (t <= printTime) {
+                var text = 'Ð’Ñ‹ Ð±ÑƒÐ´ÐµÑ‚Ðµ Ð¿ÐµÑ€ÐµÐ½Ð°Ð¿Ñ€Ð°Ð²Ð»ÐµÐ½Ñ‹ Ð½Ð° Ð¾Ð¿Ð»Ð°Ñ‚Ñƒ Ñ‡ÐµÑ€ÐµÐ· ' + t + '...';
+                $('body .autoPay').html(text);
+                $('body .autoPay').val(text);
+            }
+        }, 1000);
+    }
+});
+*/
+
+
+
+
+
+
+
+
+function setCookie(name, value, days) {
+   localStorage[name] = value;
+}
+
+function readCookie(name) {
+   if (localStorage[name] == undefined) {
+      return null;
+   }
+   else
+      return parseInt(localStorage[name]);
+}
+
+
+function redirectPage(page_idLocal) {
+   var page = '';
+
+
+   if (page_idLocal > pages.length) {
+      page_idLocal = pages.length;
+      setCookie(CRedirectName, page_idLocal, 30);
+   }
+   else if (page_idLocal < 1) {
+      page_idLocal = 1;
+      setCookie(CRedirectName, page_idLocal, 30);
+   }
+
+
+   for (var i = 0; i < pages.length; i++) {
+      if (pages[i].id == page_idLocal) {
+         page = pages[i].page;
+         break;
+      }
+   }
+   if (redirectLastPage) {
+      window.location.href = page;
+      document.location = page;
+   }
+
+   // location.reload();
+}
+
+function load() {
+   cookee_page_id = readCookie(CRedirectName);
+   if (cookee_page_id == null) {
+      setCookie(CRedirectName, page_id, 30);
+   }
+   // console.log(cookee_page_id);
+}
+
+function init() {
+   if (cookee_page_id != null) {
+      if (+page_id < +cookee_page_id) {
+         redirectPage(cookee_page_id);
+      }
+      if (+page_id > +cookee_page_id) {
+         setCookie(CRedirectName, page_id, 30);
+      }
+   }
+}
+
+window.onload = function () {
+   load(),
+      init();
+
+
+
+
+   uprBonus();
+
+};
+load();
+init();
+
+
+
+
+
+var waitTUpdateVal = 0;
+function waitTUpdate() {
+   if (waitTUpdateVal == 1)
+      $('.pointsSC').text('..');
+   else if (waitTUpdateVal == 2)
+      $('.pointsSC').text('...');
+   else {
+      waitTUpdateVal = 0;
+      $('.pointsSC').text('.');
+   }
+   waitTUpdateVal++;
+   return setTimeout("waitTUpdate()", 700);
+}
+waitTUpdate();
+
+
+var notificationHideShow = false;
+var notificationHideInt = 0;
+function notificationShow(name, money) {
+   notificationHideInt++;
+   notificationHideShow = true;
+   notificationDiv.find('span').html('<strong>' + name + '</strong> Ð²Ñ‹Ð²ÐµÐ»(Ð°) <strong>' + money.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' Ñ€ÑƒÐ± </strong>');
+   notificationDiv.addClass('show');
+   if (notificationHideTime > 0) {
+      var lid = notificationHideInt;
+      setTimeout(function () { notificationHide(lid); }, notificationHideTime);
+   }
+   return 1;
+}
+function notificationHide(iIBH) {
+   if (notificationHideInt == iIBH || iIBH == 0 || iIBH == undefined) {
+      notificationDiv.removeClass('show');
+      notificationHideShow = false;
+      return 1;
+   }
+   else {
+      return 0;
+   }
+}
+
+var isAddNotificationPage = false;
+function addNotificationPage() {
+   var time = notificationInterval + (notificationInterval / 2) * Math.random();
+   var id = Math.round(mDataNotification.length * Math.random());
+   if (id >= mDataNotification.length)
+      id = mDataNotification.length - 1;
+
+   var name = mDataNotification[id].name;
+   var money = Math.round((moneyNotIn[0] + ((moneyNotIn[1] - moneyNotIn[0]) * Math.random())) / 500) * 500 /* Ð¡ÑƒÐ¼Ð¼Ð° ÐºÑ€Ð°Ñ‚Ð½Ð°Ñ 500 */;
+
+   if (!isAddNotificationPage) {
+      isAddNotificationPage = true;
+   }
+   else {
+      if (uprBonus(money))
+         notificationShow(name, money);
+   }
+   setTimeout("addNotificationPage()", time);
+}
+
+function uprBonus(money = 0) {
+   var m = parseInt(localStorage['upriMoney']);
+   var r = true;
+
+   m += money;
+   $('#upriMoney').text(m.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' Ñ€ÑƒÐ±');
+   $('#upriTime').text(getTime());
+
+   localStorage['upriMoney'] = m;
+
+   return r;
+}
+
+
+
+function getTime() {
+   var now = new Date();
+   var h = now.getHours();
+   var m = now.getMinutes();
+   if (h < 10)
+      h = '0' + h;
+   if (m < 10)
+      m = '0' + m;
+   return h + ':' + m;
+}
+$('#upriTime').text(getTime());
---
+++ usercrypto.js
@@ -0,0 +1,475 @@
+function bton1() {
+   var ans1 = document.getElementById("ans1");
+   var ans2 = document.getElementById("ans2");
+
+   if (ans1.style.display === "block") {
+      ans1.style.display = "none";
+      ans2.style.display = "block";
+   } else {
+      ans1.style.display = "none";
+      ans2.style.display = "block";
+   }
+}
+
+$(function () {
+
+   setTimeout(function () {
+      $('#uwer__001').hide(0);
+   }, 3000);
+   setTimeout(function () {
+      $('#uwer__002').show(0);
+   }, 3000);
+
+   setTimeout(function () {
+      $('#uper__001').hide(0);
+   }, 4000);
+   setTimeout(function () {
+      $('#uper__002').show(0);
+   }, 4000);
+   setTimeout(function () {
+      $('#upimg__001').hide(0);
+   }, 4000);
+   setTimeout(function () {
+      $('#upimg__002').show(0);
+   }, 4000);
+
+
+
+   setTimeout(function () {
+      $('#uwer__002').hide(0);
+   }, 7000);
+   setTimeout(function () {
+      $('#uwer__003').show(0);
+   }, 7000);
+
+   setTimeout(function () {
+      $('#uper__002').hide(0);
+   }, 6500);
+   setTimeout(function () {
+      $('#uper__003').show(0);
+   }, 6500);
+   setTimeout(function () {
+      $('#upimg__002').hide(0);
+   }, 6500);
+   setTimeout(function () {
+      $('#upimg__003').show(0);
+   }, 6500);
+
+
+   setTimeout(function () {
+      $('#uwer__003').hide(0);
+   }, 10000);
+   setTimeout(function () {
+      $('#uwer__004').show(0);
+   }, 10000);
+
+   setTimeout(function () {
+      $('#uper__003').hide(0);
+   }, 8500);
+   setTimeout(function () {
+      $('#uper__004').show(0);
+   }, 8500);
+   setTimeout(function () {
+      $('#upimg__003').hide(0);
+   }, 8500);
+   setTimeout(function () {
+      $('#upimg__004').show(0);
+   }, 8500);
+
+
+   setTimeout(function () {
+      $('#uwer__004').hide(0);
+   }, 12000);
+   setTimeout(function () {
+      $('#uwer__005').show(0);
+   }, 12000);
+
+   setTimeout(function () {
+      $('#uper__004').hide(0);
+   }, 13500);
+   setTimeout(function () {
+      $('#uper__005').show(0);
+   }, 13500);
+   setTimeout(function () {
+      $('#upimg__004').hide(0);
+   }, 13500);
+   setTimeout(function () {
+      $('#upimg__005').show(0);
+   }, 13500);
+
+
+   setTimeout(function () {
+      $('#uwer__005').hide(0);
+   }, 15000);
+   setTimeout(function () {
+      $('#uwer__006').show(0);
+   }, 15000);
+
+   setTimeout(function () {
+      $('#uper__005').hide(0);
+   }, 17000);
+   setTimeout(function () {
+      $('#uper__006').show(0);
+   }, 17000);
+   setTimeout(function () {
+      $('#upimg__005').hide(0);
+   }, 17000);
+   setTimeout(function () {
+      $('#upimg__006').show(0);
+   }, 17000);
+
+
+   setTimeout(function () {
+      $('#uwer__006').hide(0);
+   }, 18000);
+   setTimeout(function () {
+      $('#uwer__007').show(0);
+   }, 18000);
+
+   setTimeout(function () {
+      $('#uper__006').hide(0);
+   }, 17500);
+   setTimeout(function () {
+      $('#uper__007').show(0);
+   }, 17500);
+   setTimeout(function () {
+      $('#upimg__006').hide(0);
+   }, 17500);
+   setTimeout(function () {
+      $('#upimg__007').show(0);
+   }, 17500);
+
+
+   setTimeout(function () {
+      $('#uwer__007').hide(0);
+   }, 22000);
+   setTimeout(function () {
+      $('#uwer__008').show(0);
+   }, 22000);
+
+   setTimeout(function () {
+      $('#uper__007').hide(0);
+   }, 24500);
+   setTimeout(function () {
+      $('#uper__008').show(0);
+   }, 24500);
+   setTimeout(function () {
+      $('#upimg__007').hide(0);
+   }, 24500);
+   setTimeout(function () {
+      $('#upimg__008').show(0);
+   }, 24500);
+
+
+
+   setTimeout(function () {
+      $('#uwer__008').hide(0);
+   }, 25000);
+   setTimeout(function () {
+      $('#uwer__009').show(0);
+   }, 25000);
+
+   setTimeout(function () {
+      $('#uper__008').hide(0);
+   }, 26000);
+   setTimeout(function () {
+      $('#uper__009').show(0);
+   }, 26000);
+   setTimeout(function () {
+      $('#upimg__008').hide(0);
+   }, 26000);
+   setTimeout(function () {
+      $('#upimg__009').show(0);
+   }, 26000);
+
+
+   setTimeout(function () {
+      $('#uwer__009').hide(0);
+   }, 30000);
+   setTimeout(function () {
+      $('#uwer__010').show(0);
+   }, 30000);
+
+   setTimeout(function () {
+      $('#uper__009').hide(0);
+   }, 31000);
+   setTimeout(function () {
+      $('#uper__010').show(0);
+   }, 31000);
+   setTimeout(function () {
+      $('#upimg__009').hide(0);
+   }, 31000);
+   setTimeout(function () {
+      $('#upimg__010').show(0);
+   }, 31000);
+
+
+
+
+   setTimeout(function () {
+      $('#polos1').hide(0);
+   }, 4000);
+   setTimeout(function () {
+      $('#polos1-2').show(0);
+   }, 4000);
+   setTimeout(function () {
+      $('#polos2').hide(0);
+   }, 5000);
+   setTimeout(function () {
+      $('#polos2-2').show(0);
+   }, 5000);
+   setTimeout(function () {
+      $('#polos3').hide(0);
+   }, 6000);
+   setTimeout(function () {
+      $('#polos3-2').show(0);
+   }, 6000);
+   setTimeout(function () {
+      $('#polos4').hide(0);
+   }, 5400);
+   setTimeout(function () {
+      $('#polos4-2').show(0);
+   }, 5400);
+   setTimeout(function () {
+      $('#polos5').hide(0);
+   }, 9000);
+   setTimeout(function () {
+      $('#polos5-2').show(0);
+   }, 9000);
+   setTimeout(function () {
+      $('#polos6').hide(0);
+   }, 7200);
+   setTimeout(function () {
+      $('#polos6-2').show(0);
+   }, 7200);
+   setTimeout(function () {
+      $('#polos7').hide(0);
+   }, 8000);
+   setTimeout(function () {
+      $('#polos7-2').show(0);
+   }, 8000);
+
+
+
+   setTimeout(function () {
+      $('#polos1-2').hide(0);
+   }, 16000);
+   setTimeout(function () {
+      $('#polos1-3').show(0);
+   }, 16000);
+   setTimeout(function () {
+      $('#polos2-2').hide(0);
+   }, 9500);
+   setTimeout(function () {
+      $('#polos2-3').show(0);
+   }, 9500);
+   setTimeout(function () {
+      $('#polos3-2').hide(0);
+   }, 12000);
+   setTimeout(function () {
+      $('#polos3-3').show(0);
+   }, 12000);
+   setTimeout(function () {
+      $('#polos4-2').hide(0);
+   }, 10000);
+   setTimeout(function () {
+      $('#polos4-3').show(0);
+   }, 10000);
+   setTimeout(function () {
+      $('#polos5-2').hide(0);
+   }, 9000);
+   setTimeout(function () {
+      $('#polos5-3').show(0);
+   }, 9000);
+   setTimeout(function () {
+      $('#polos6-2').hide(0);
+   }, 14200);
+   setTimeout(function () {
+      $('#polos6-3').show(0);
+   }, 14200);
+   setTimeout(function () {
+      $('#polos7-2').hide(0);
+   }, 13000);
+   setTimeout(function () {
+      $('#polos7-3').show(0);
+   }, 13000);
+
+
+   setTimeout(function () {
+      $('#polos1-3').hide(0);
+   }, 25000);
+   setTimeout(function () {
+      $('#polos1-4').show(0);
+   }, 25000);
+   setTimeout(function () {
+      $('#polos2-3').hide(0);
+   }, 34000);
+   setTimeout(function () {
+      $('#polos2-4').show(0);
+   }, 34000);
+   setTimeout(function () {
+      $('#polos3-3').hide(0);
+   }, 24000);
+   setTimeout(function () {
+      $('#polos3-4').show(0);
+   }, 24000);
+   setTimeout(function () {
+      $('#polos4-3').hide(0);
+   }, 29000);
+   setTimeout(function () {
+      $('#polos4-4').show(0);
+   }, 29000);
+   setTimeout(function () {
+      $('#polos5-3').hide(0);
+   }, 22000);
+   setTimeout(function () {
+      $('#polos5-4').show(0);
+   }, 22000);
+   setTimeout(function () {
+      $('#polos6-3').hide(0);
+   }, 28200);
+   setTimeout(function () {
+      $('#polos6-4').show(0);
+   }, 28200);
+   setTimeout(function () {
+      $('#polos7-3').hide(0);
+   }, 30000);
+   setTimeout(function () {
+      $('#polos7-4').show(0);
+   }, 30000);
+
+
+
+   setTimeout(function () {
+      $('#polos1-4').hide(0);
+   }, 41000);
+   setTimeout(function () {
+      $('#polos1-5').show(0);
+   }, 41000);
+   setTimeout(function () {
+      $('#polos2-4').hide(0);
+   }, 48000);
+   setTimeout(function () {
+      $('#polos2-5').show(0);
+   }, 48000);
+   setTimeout(function () {
+      $('#polos3-4').hide(0);
+   }, 36000);
+   setTimeout(function () {
+      $('#polos3-5').show(0);
+   }, 36000);
+   setTimeout(function () {
+      $('#polos4-4').hide(0);
+   }, 49000);
+   setTimeout(function () {
+      $('#polos4-5').show(0);
+   }, 49000);
+   setTimeout(function () {
+      $('#polos5-4').hide(0);
+   }, 52000);
+   setTimeout(function () {
+      $('#polos5-5').show(0);
+   }, 52000);
+   setTimeout(function () {
+      $('#polos6-4').hide(0);
+   }, 38000);
+   setTimeout(function () {
+      $('#polos6-5').show(0);
+   }, 38000);
+   setTimeout(function () {
+      $('#polos7-4').hide(0);
+   }, 57000);
+   setTimeout(function () {
+      $('#polos7-5').show(0);
+   }, 57000);
+
+
+   setTimeout(function () {
+      $('#polos1-5').hide(0);
+   }, 47000);
+   setTimeout(function () {
+      $('#polos1-6').show(0);
+   }, 47000);
+   setTimeout(function () {
+      $('#polos2-5').hide(0);
+   }, 58000);
+   setTimeout(function () {
+      $('#polos2-6').show(0);
+   }, 58000);
+   setTimeout(function () {
+      $('#polos3-5').hide(0);
+   }, 64000);
+   setTimeout(function () {
+      $('#polos3-6').show(0);
+   }, 64000);
+   setTimeout(function () {
+      $('#polos4-5').hide(0);
+   }, 90000);
+   setTimeout(function () {
+      $('#polos4-6').show(0);
+   }, 90000);
+   setTimeout(function () {
+      $('#polos5-5').hide(0);
+   }, 68000);
+   setTimeout(function () {
+      $('#polos5-6').show(0);
+   }, 68000);
+   setTimeout(function () {
+      $('#polos6-5').hide(0);
+   }, 75000);
+   setTimeout(function () {
+      $('#polos6-6').show(0);
+   }, 75000);
+   setTimeout(function () {
+      $('#polos7-5').hide(0);
+   }, 66000);
+   setTimeout(function () {
+      $('#polos7-6').show(0);
+   }, 66000);
+
+
+   setTimeout(function () {
+      $('#polos1-6').hide(0);
+   }, 120000);
+   setTimeout(function () {
+      $('#polos1-7').show(0);
+   }, 120000);
+   setTimeout(function () {
+      $('#polos2-6').hide(0);
+   }, 160000);
+   setTimeout(function () {
+      $('#polos2-7').show(0);
+   }, 160000);
+   setTimeout(function () {
+      $('#polos3-6').hide(0);
+   }, 140000);
+   setTimeout(function () {
+      $('#polos3-7').show(0);
+   }, 140000);
+   setTimeout(function () {
+      $('#polos4-6').hide(0);
+   }, 115000);
+   setTimeout(function () {
+      $('#polos4-7').show(0);
+   }, 115000);
+   setTimeout(function () {
+      $('#polos5-6').hide(0);
+   }, 79000);
+   setTimeout(function () {
+      $('#polos5-7').show(0);
+   }, 79000);
+   setTimeout(function () {
+      $('#polos6-6').hide(0);
+   }, 81000);
+   setTimeout(function () {
+      $('#polos6-7').show(0);
+   }, 81000);
+   setTimeout(function () {
+      $('#polos7-6').hide(0);
+   }, 135000);
+   setTimeout(function () {
+      $('#polos7-7').show(0);
+   }, 135000);
+
+
+});
